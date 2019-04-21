8c9140ee3d7c8ecfa895bcfabed92e5d92573638
Andrew Silva <andrewsilva9@gmail.com>
2019-04-21 20:41:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def batch_badge_creator(name)
 end
 
 def assign_rooms(name)
-  counter = 1
+  counter = 0
   name.map do |speaker|
     counter += 1
     "Hello, #{speaker}! You'll be assigned to room #{counter}"
