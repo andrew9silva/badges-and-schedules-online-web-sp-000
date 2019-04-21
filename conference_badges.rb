fc80631245557758e21d8df70a45666ceaa73ca7
Andrew Silva <andrewsilva9@gmail.com>
2019-04-21 19:47:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,10 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(name)
+  name = []
+  name.each do |speaker|
+    puts "Hello, my name is #{speaker}."
+  end
+end
